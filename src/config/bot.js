10fb70192df993e8251af1f5ac1ecd90dad38ac4
Aly Guevarra <alysajuliengvrr@gmail.com>
2026-07-01 02:46:18 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -280,7 +280,7 @@ export const botConfig = {
     defaultRole: null,
 
     // Channel ID where birthday announcements are posted.
-    announcementChannel: null,
+    announcementChannel: 1521579688252674260,
 
     // Timezone used to calculate birthday dates.
     timezone: "UTC",
